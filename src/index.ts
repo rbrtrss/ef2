cb4376c4636d6de193a8d701d2b42e6f7c3ba531
rbrtrss <roberto.rousse@gmail.com>
2021-12-02 20:01:29 -0300

    add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import router from './routes';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import { parserConfiguration } from 'yargs';
+
+const parser = yargs(hideBin(process.argv)).options({
+  port: { type: 'string', default: '8080' },
+});
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+// const port = argv.port || 8080;
 
-app.listen(port, () => {
-  console.log(`Servidor UP en ${port}`);
+const port = async () => {
+  const argv = await parser.argv;
+  return argv.port;
+};
+
+app.listen(port(), () => {
+  console.log(`Servidor UP en ${port()}`);
 });
 
 app.use(express.json());
